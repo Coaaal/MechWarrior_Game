9b4cac83f2bfcd6c7a182113c76c8da2c6d99ef4
Coaaal <coaaal@gmail.com>
2016-11-17 23:12:34 -0800

    FrameworksUpdate
    Infinite Tiling Achieved!

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ class Game:
         for a in range(int(self.render_surface.rect.height/TILE_SIZE) + 1):
             for b in range(int(self.render_surface.rect.width/TILE_SIZE)):
                 self.floor_tile = GameEntity(self, TILE_SIZE, TILE_SIZE)
+                if b == 4 and a == 4:
+                    self.floor_tile = GameEntity(self, TILE_SIZE, TILE_SIZE, DESSERT)
                 self.offset_x = (self.render_surface.rect.x + TILE_SIZE * b)
                 self.offset_y = (self.render_surface.rect.y + TILE_SIZE * a)
                 self.floor_tile.rect.x = self.offset_x
@@ -107,7 +109,7 @@ class Game:
         previous_sprite_count = -1
         next_sprite_count = 1
         currCount = 0
-        previous_height_sprite = int(self.render_surface.rect.width/TILE_SIZE)
+        amount_tiles_wide = int(self.render_surface.rect.width/TILE_SIZE)
         for current_sprite in self.all_sprites.sprites():
             current_sprite.rect.x += int(self.vel.x + 0.5 * self.acc.x)
             current_sprite.rect.y += int(self.vel.y + 0.5 * self.acc.y)
@@ -122,25 +124,36 @@ class Game:
             #     current_sprite.rect.y = self.render_surface.rect.y
             # previous_sprite_count += 1
         for current_sprite in self.all_sprites.sprites():
+            position_modifier1 = currCount
+            position_modifier2 = currCount
             if next_sprite_count == len(self.all_sprites):
                 next_sprite_count = 0
-            previous_height_sprite = currCount - previous_height_sprite
-            previous_sprite_x_right = (self.all_sprites.sprites()[previous_sprite_count].rect.right) + 1
-            next_sprite_x_left = (self.all_sprites.sprites()[next_sprite_count].rect.left) - 1
-            previous_sprite_y_bot = (self.all_sprites.sprites()[previous_height_sprite].rect.bottom) + 1
+
+            previous_sprite_x_right = (self.all_sprites.sprites()[previous_sprite_count].rect.right)
+            next_sprite_x_left = (self.all_sprites.sprites()[next_sprite_count].rect.left)
+
             # previous_sprite_x_bot = (self.all_sprites.sprites()[previous_sprite_count].rect.bottom) + 1
             if current_sprite.rect.x < self.render_surface.rect.x:
                 current_sprite.rect.x = previous_sprite_x_right
-            if current_sprite.rect.x > self.render_surface.rect.x + self.render_surface.rect.width:
-                current_sprite.rect.right = next_sprite_x_left + 1
-            if current_sprite.rect.y < self.render_surface.rect.y:
+            if current_sprite.rect.right > self.render_surface.rect.x + self.render_surface.rect.width:
+                current_sprite.rect.right = next_sprite_x_left
+            if current_sprite.rect.top - 1 < self.render_surface.rect.y:
+                for a in range(amount_tiles_wide):
+                    position_modifier1 -= 1
+                    if position_modifier1 == -1:
+                        position_modifier1 = len(self.all_sprites.sprites()) - 1
+                previous_sprite_y_bot = (self.all_sprites.sprites()[position_modifier1].rect.bottom)
                 current_sprite.rect.top = previous_sprite_y_bot
-            if current_sprite.rect.y > self.render_surface.rect.y + self.render_surface.rect.height:
-                current_sprite.rect.y = self.render_surface.rect.y
+            if current_sprite.rect.bottom > self.render_surface.rect.y + self.render_surface.rect.height:
+                for b in range(amount_tiles_wide):
+                    position_modifier2 += 1
+                    if position_modifier2 == len(self.all_sprites):
+                        position_modifier2 = 0
+                next_sprite_y_top = (self.all_sprites.sprites()[position_modifier2].rect.top)
+                current_sprite.rect.bottom = next_sprite_y_top
             previous_sprite_count += 1
             next_sprite_count += 1
-            previous_height_sprite += 1
-            currCount
+            currCount += 1
             # self.render_surface.blit(self.floor_tile.image, (self.offset_x, self.offset_y), None)
         self.all_sprites.update()
         self.player_sprite.update()
